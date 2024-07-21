f040e99564404b6c7aab041fa820127b52e4d77f
coder049 <saikumarreddy049@gmail.com>
2024-07-21 15:43:00 +0530

    deployment commit

--- frontend/src/components/register.jsx
+++ frontend/src/components/register.jsx
@@ -18,7 +18,7 @@ const RegisterForm = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("http://co2-footprint.onrender.com/api/auth/register", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
